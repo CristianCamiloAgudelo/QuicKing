242aac2a8eef991438e04d2d6ec057a70bba3500
CristianCamiloAgudelo <cristian.1701322386@ucaldas.edu.co>
2020-03-25 20:59:59 -0500

    Creación de estructura web, Common, Prism, Xamarin Forms

---
+++ QuicKing.Web/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuicKing.Web.Data.Entities;
+
+namespace QuicKing.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TaxiEntity> Taxis { get; set; }
+
+    }
+
+}
+
