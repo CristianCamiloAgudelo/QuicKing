b145d2d4d1f6840c825afa030fd1460203734fa8
CristianCamiloAgudelo <cristian.1701322386@ucaldas.edu.co>
2020-05-07 18:00:13 -0500

    GetMyTrips obtener los viajes de un usuario en un rango de fecha

---
+++ QuicKing.Common/Models/TripResponseWithTaxi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuicKing.Common.Models
+{
+    public class TripResponseWithTaxi : TripResponse
+    {
+        public TaxiResponse Taxi { get; set; }
+    }
+
+}
--- QuicKing.Web/Helpers/IConverterHelper.cs
+++ QuicKing.Web/Helpers/IConverterHelper.cs
@@ -1,10 +1,13 @@
 ﻿using QuicKing.Common.Models;
 using QuicKing.Web.Data.Entities;
+using System.Collections.Generic;
 
 namespace QuicKing.Web.Helpers
 {
     public interface IConverterHelper
     {
+        List<TripResponseWithTaxi> ToTripResponse(List<TripEntity> tripEntities);
+
         TaxiResponse ToTaxiResponse(TaxiEntity taxiEntity);
 
         TripResponse ToTripResponse(TripEntity tripEntity);
