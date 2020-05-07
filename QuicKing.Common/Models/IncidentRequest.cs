3a76ec493613d28b12d289e9cb6d47d2b6a22a01
CristianCamiloAgudelo <cristian.1701322386@ucaldas.edu.co>
2020-05-07 11:13:36 -0500

    API para completar un viaje(TRIP)

---
+++ QuicKing.Common/Models/IncidentRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace QuicKing.Common.Models
+{
+    public class IncidentRequest : TripRequest
+    {
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Remarks { get; set; }
+    }
+
+}
